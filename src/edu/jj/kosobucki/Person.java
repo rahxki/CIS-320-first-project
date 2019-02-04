2b86b3c8e6f08324ca9f2fd3bb3cbd855eadbadf
rahxki <icemetal1>
2019-02-04 15:53:55 -0600

    fixing upload issues

---
+++ src/edu/jj/kosobucki/Person.java
@@ -0,0 +1,16 @@
+package edu.jj.kosobucki;
+
+public class Person {
+
+    private int id;
+    private String first;
+    private String last;
+    private String phone;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() {return first; }
+    public void setFirst(String first) {this.first = first; }
+    public void setLast(String last) {this.last = last; }
+}
