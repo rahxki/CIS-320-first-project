268f5ed04249ecc9731b85ac4f68f3057de0032e
J.J. Kosobucki <jfkosobucki@gmail.com>
2019-02-04 16:22:53 -0600

    Add files via upload
    Fixing issues

--- src/edu/jj/kosobucki/NameListGet.java
+++ src/edu/jj/kosobucki/NameListGet.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.jj.kosobucki;
 
 import javax.servlet.ServletException;
@@ -33,40 +32,4 @@ public class NameListGet extends HttpServlet {
         out.println(json);
 
     }
-=======
-package edu.jj.kosobucki;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import com.google.gson.Gson;
-
-@WebServlet(name = "NameListGet")
-public class NameListGet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        // Get setup up to output JSON text
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
-
-        // Use our DAO to get a list of people
-        List <Person> peopleList = PersonDAO.getPeople();
-
-        // Create an instance of the Google class that can convert
-        // business object to JSON strings.
-        Gson gson = new Gson();
-
-        // Go ahead and turn peopleList into a JSON string
-        String json = gson.toJson(peopleList);
-
-        // Write out that string
-        out.println(json);
-
-    }
->>>>>>> 7165c9fdd304d19d288f5e585479135b5722ed13
 }
